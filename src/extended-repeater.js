ace850da751a588c1a99580cba90578a6503ceb8
Vladimir Riz <resin.fhp@gmail.com>
2020-09-21 17:03:15 +0300

    feat: resolve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,81 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = str !== undefined ? String(str) : "";
+  options.addition =
+    options.addition !== undefined ? String(options.addition) : "";
+
+  options.separator = options.separator ? options.separator : "+";
+  options.additionSeparator = options.additionSeparator
+    ? options.additionSeparator
+    : "|";
+  options.repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  options.additionRepeatTimes = options.additionRepeatTimes
+    ? options.additionRepeatTimes
+    : 1;
+
+  console.log(options.repeatTimes);
+
+  let line = "";
+  let arr = [];
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    line += `${str}`;
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      if (options.additionRepeatTimes > 0) {
+        line += `${options.addition}${options.additionSeparator}`;
+      } else line += `${options.addition}`;
+    }
+    line += "/";
+  }
+  let divide = line.split("/");
+  console.log(divide);
+  divide.pop();
+
+  for (e of divide) {
+    if (options.additionRepeatTimes > 0) {
+      arr.push(e.slice(0, -`${options.additionSeparator.length}`));
+    } else arr.push(e);
+  }
+  return arr.join(options.separator);
 };
-  
+
+const repeater = (str, options) => {
+  str = str !== undefined ? String(str) : "";
+  options.addition =
+    options.addition !== undefined ? String(options.addition) : "";
+
+  options.separator = options.separator ? options.separator : "+";
+  options.additionSeparator = options.additionSeparator
+    ? options.additionSeparator
+    : "|";
+  options.repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  options.additionRepeatTimes = options.additionRepeatTimes
+    ? options.additionRepeatTimes
+    : 1;
+
+  console.log(options.repeatTimes);
+
+  let line = "";
+  let arr = [];
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    line += `${str}`;
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      if (options.additionRepeatTimes > 0) {
+        line += `${options.addition}${options.additionSeparator}`;
+      } else line += `${options.addition}`;
+    }
+    line += "/";
+  }
+  let divide = line.split("/");
+  console.log(divide);
+  divide.pop();
+
+  for (e of divide) {
+    if (options.additionRepeatTimes > 0) {
+      arr.push(e.slice(0, -`${options.additionSeparator.length}`));
+    } else arr.push(e);
+  }
+  return arr.join(options.separator);
+};
